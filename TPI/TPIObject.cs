e8479c6373d3354e7bcbb3c7a445933ca29a5b09
Peter Chen <peter_chen@trimble.com>
2015-04-02 19:59:46 +0800

    Delete interface file

--- TPI/TPIObject.cs
+++ TPI/TPIObject.cs
@@ -3,6 +3,51 @@ using System.Threading.Tasks;
 
 namespace TPI
 {
+    internal interface ICanShow
+    {
+        Task<string> Show();
+    }
+
+    internal interface ICanShow<T> : ICanShow
+    {
+        new Task<T> Show();
+    }
+
+    internal interface ICanSet
+    {
+
+    }
+
+    internal interface ICanReset
+    {
+
+    }
+
+    internal interface ICanEnable
+    {
+
+    }
+
+    internal interface ICanDisable
+    {
+
+    }
+
+    internal interface ICanDelete
+    {
+
+    }
+
+    internal interface ICanDownload
+    {
+
+    }
+
+    internal interface ICanUpload
+    {
+
+    }
+
     internal class TPIObject
     {
         protected TPIReceiver receiver;
